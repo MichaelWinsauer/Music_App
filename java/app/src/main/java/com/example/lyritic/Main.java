ecd1ec940c804d9380eda114f39ad8ec4f241e8e
MichaelWinsauer <michael@winsauer.de>
2020-02-03 08:38:20 +0100

    Initial Commit - Java Projekt created

---
+++ java/app/src/main/java/com/example/lyritic/Main.java
@@ -0,0 +1,13 @@
+package com.example.lyritic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse);
+
+    }
+}
