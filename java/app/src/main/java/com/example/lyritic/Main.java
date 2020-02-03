2a54e4cb7fa42cafbb704c2ce0c1e6527fafec1f
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2020-02-03 20:20:14 +0100

    Implemented Song-Class & created Test-Mockup

--- java/app/src/main/java/com/example/lyritic/Main.java
+++ java/app/src/main/java/com/example/lyritic/Main.java
@@ -1,13 +1,44 @@
 package com.example.lyritic;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse);
+
+        LinearLayout llSongList = (LinearLayout) findViewById(R.id.llSongList);
+
+        List<Song> songList = new ArrayList<>();
+
+
+        for(int i = 0; i < 30; i++) {
+            songList.add(new Song());
+        }
+
+        for(Song s : songList) {
+            Button b = new Button(this);
+            b.setText("Button " + s.getId());
+            b.setBackgroundColor(Color.rgb(40, 53, 64));
+            b.setTextColor(Color.rgb(230, 230, 230));
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 5, 0, 5);
+
+            llSongList.addView(b, layoutParams);
+        }
 
     }
 }
---
+++ java/app/src/main/java/com/example/lyritic/Song.java
@@ -0,0 +1,113 @@
+package com.example.lyritic;
+
+public class Song {
+
+    private static int count;
+
+    private String title;
+    private String interpret;
+    private String album;
+    private String genre;
+    private String path;
+    private String format;
+
+    private double length;
+    private double size;
+
+    private int number;
+    private int id;
+
+    public Song()
+    {
+        count++;
+        id = count;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Song.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInterpret() {
+        return interpret;
+    }
+
+    public void setInterpret(String interpret) {
+        this.interpret = interpret;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
